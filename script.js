994717c778b71d7a52a11b11d3391c8a2f4e7d77
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-04-20 04:41:39 -0300

    versão com a primeira parte da tela2

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 axios.defaults.headers.common['Authorization'] = 'thWBkfA2HxJeEDBnysTfueIT';
+let tela1 = document.querySelector(".tela1");
+let tela2 = document.querySelector(".tela2");
+let tela3 = document.querySelector(".tela3");
+
+// tela 2 - página de um quiz
+
+let promisse = axios.get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes");
+promisse.then((resposta)=>{
+    console.log(resposta.data);
+});
+promisse.catch((erro)=>{
+    console.log(erro.data);
+});
+
+function selecionaQuiz(quiz){
+    let id = quiz.id;
+    let promessaQuiz = axios.get(`https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes/${id}`);
+    promessaQuiz.then(carregaQuiz);
+}   
+
+function carregaQuiz(resposta){
+    console.log(resposta);
+    tela1.classList.add("escondido");
+    tela2.classList.remove("escondido");
+    tela3.classList.add("escondido");
+    const tituloQuiz = resposta.data.title;
+    const imgQuiz = resposta.data.image;
+    const perguntas = resposta.data.questions;
+    tela2.innerHTML += `
+        <div class="header-Quiz">
+            <img src="${imgQuiz}"/>
+            <p>${tituloQuiz}</p>
+        </div>
+    `;
+    for (let i = 0; i < perguntas.length; i++) {
+        tela2.innerHTML += `
+            <div class="container-perguntas">
+                <div style="background-color:${perguntas[i].color};"  class="pergunta">${perguntas[i].title}</div>
+                <div id ="${i}" class="respostas"></div>
+            </div>
+        `;
+        perguntas[i].answers.sort(comparador);
+        for (let j = 0; j < perguntas[i].answers.length; j++) {
+            let respostas = document.getElementById(i);
+            respostas.innerHTML += `
+            <div class="resposta">
+                <img src="${perguntas[i].answers[j].image}"/>
+                <p>${perguntas[i].answers[j].text}</p>
+            </div>
+            `
+        }
+    }
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+let novoQuiz = {
+    id: 3,
+};
+selecionaQuiz(novoQuiz);
